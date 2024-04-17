eaaa9cc237581e731046f380f7eb84f942f374c9
Hussein Tirawi <32430109+htirawi@users.noreply.github.com>
2024-04-17 17:18:24 +0300

    finding longest subarray length using sliding window (#4)

---
+++ 02-sliding-window/longest-subarray-length/longest-subarray-length-run.js
@@ -0,0 +1,7 @@
+const longestSubArrayLength = require('./longest-subarray-length');
+
+const arr = [3, 1, 2, 7, 4, 2, 1, 1, 5];
+
+const result = longestSubArrayLength(arr, 8);
+
+console.log(result);
