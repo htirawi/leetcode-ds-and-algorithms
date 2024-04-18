56ac9e27303fea5623424be4542a5b0b480a6b7a
Hussein Tirawi <32430109+htirawi@users.noreply.github.com>
2024-04-18 12:04:25 +0300

    longest subarray sum (#6)

---
+++ 02-sliding-window/02.1-fixed-window-size/longest-subarray-sum/largest-subarray-sum-run.js
@@ -0,0 +1,8 @@
+const largestSubArraySum = require('./largest-subarray-sum');
+
+const arr = [2, 5, 3, 1, 11, 7, 6, 4];
+const k = 3;
+
+const result = largestSubArraySum(arr, k);
+
+console.log(result);
---
+++ 02-sliding-window/02.1-fixed-window-size/longest-subarray-sum/largest-subarray-sum.test.js
@@ -0,0 +1,11 @@
+const largestSubArraySum = require('./largest-subarray-sum');
+
+test('Finding maximum subarray sum using O(n) solution', () => {
+  const arr1 = [2, 5, 3, 1, 11, 7, 6, 4];
+  const k1 = 3;
+  expect(largestSubArraySum(arr1, k1)).toBe(24);
+
+  const arr2 = [-2, -5, -3, -1, -11, -7, -6, -4];
+  const k2 = 4;
+  expect(largestSubArraySum(arr2, k2)).toBe(-11);
+});
